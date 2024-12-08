dbec03f94f04882cc9ac056aff105a45b8d16f1e
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-08 21:05:10 +0000

    feat : robtoy dir changer smooth turn

--- scenes/RobDirChanger/RobDirChanger.gd
+++ scenes/RobDirChanger/RobDirChanger.gd
@@ -20,21 +20,35 @@ enum FACING_DIR { UP=0, RIGHT=1, DOWN=2, LEFT=3 }
 
 var direction : Vector2 = Vector2.RIGHT :
 	set(value) :
+		update_body(direction, value)
 		direction = value.normalized()
-		update_body()
+var focused := false
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
-func update_body() :
-	$Body/Arrow.look_at(position+direction)
+func _process(_delta: float) -> void:
+	if Engine.is_editor_hint() :
+		global_position = snapped(global_position, Vector2(200, 200))
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("next") and focused :
+		next_dir()
+
+func update_body(from:Vector2, to:Vector2) :
+	var tween = create_tween()
+	tween.tween_method($Body/Arrow.look_at, from, to, 0.2)
+	
+func next_dir() :
+	direction = direction.rotated(PI/2)
 
 func _on_body_entered(body:Node2D) -> void:
 	if not body is RobToy :
 		return
 	(body as RobToy).change_direction(direction)
 
-func _process(_delta: float) -> void:
-	if Engine.is_editor_hint() :
-		global_position = snapped(global_position, Vector2(200, 200))
+func _on_mouse_entered() -> void:
+	focused = true
+
+func _on_mouse_exited() -> void:
+	focused = false
