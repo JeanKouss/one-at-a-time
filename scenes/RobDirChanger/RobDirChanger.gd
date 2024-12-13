d7abb20642ffa0feb73b011dc57069a4737dcd18
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-13 22:33:06 +0000

    art : hovering direction juice

--- scenes/RobDirChanger/RobDirChanger.gd
+++ scenes/RobDirChanger/RobDirChanger.gd
@@ -17,17 +17,28 @@ enum FACING_DIR { UP=0, RIGHT=1, DOWN=2, LEFT=3 }
 			_:
 				pass
 		facing = value
-
+var body_tween : Tween = Tween.new()
 var direction : Vector2 = Vector2.RIGHT :
 	set(value) :
 		update_body(direction, value)
 		direction = value.normalized()
-var focused := false
+
+var toys_entered : Array[RobToy] = []
+var can_focus : = true
+
+var focused := false :
+	set(value) :
+		if not can_focus :
+			return
+		focused = value
+		_focus_entered() if focused else _focus_exited()
+
 
 func _ready() -> void:
 	pass
 
 func _process(_delta: float) -> void:
+	%DirArrows.material.set_shader_parameter("modulate", $Body.modulate)
 	if Engine.is_editor_hint() :
 		global_position = snapped(global_position, Vector2(200, 200))
 
@@ -41,13 +52,57 @@ func update_body(_from:Vector2, to:Vector2) :
 func next_dir() :
 	direction = direction.rotated(PI/2)
 
-func _on_body_entered(body:Node2D) -> void:
-	if not body is RobToy :
-		return
-	(body as RobToy).change_direction(direction)
-
 func _on_mouse_entered() -> void:
 	focused = true
 
 func _on_mouse_exited() -> void:
 	focused = false
+
+
+func _on_body_entered(body:Node2D) -> void:
+	if not body is RobToy :
+		return
+	toys_entered.append(body)
+	(body as RobToy).change_direction(direction)
+	lock(true)
+
+func _on_body_exited(body:Node2D) -> void:
+	if not body is RobToy :
+		return
+	toys_entered.erase(body)
+	if toys_entered.is_empty() :
+		unlock()
+
+func _focus_entered() :
+	var mod_col : Color = Color('#0094C6') * 3.
+	mod_col.a = 1.0
+	$Body/Background.visible = true
+	if body_tween.is_valid() :
+		body_tween.kill()
+	body_tween = create_tween().set_parallel(true)
+	body_tween.tween_property($Body, 'modulate', mod_col, 0.1)
+	body_tween.tween_property($Body/Background, 'scale', Vector2.ONE * 1.1, 0.1)
+	
+
+func _focus_exited() :
+	if body_tween.is_valid() :
+		body_tween.kill()
+	body_tween = create_tween().set_parallel(true)
+	body_tween.tween_property($Body, 'modulate', Color('#FFF'), 0.1)
+	body_tween.tween_property($Body/Background, 'scale', Vector2.ONE, 0.1)
+	body_tween.chain().tween_property($Body/Background, 'visible', false, 0.0)
+
+func lock(_stepped:bool=true) :
+	focused = false
+	can_focus = false
+	$Body.modulate = Color('#0FFF95') * 3
+	$Body.modulate.a = 1.0
+	# material.set_shader_parameter("some_value", some_value)
+	$Body/Background.scale = Vector2.ONE
+	$Body/Background.visible = true
+
+func unlock() :
+	can_focus = true
+	$Body.modulate = Color('#FFF')
+	$Body/Background.scale = Vector2.ONE
+	$Body/Background.visible = false
