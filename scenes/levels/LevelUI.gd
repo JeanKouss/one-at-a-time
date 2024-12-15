efa30fadcd8e336efed71aaadd4528468a95e6e4
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-15 03:14:09 +0000

    refactor level ui & added score count

--- scenes/levels/LevelUI.gd
+++ scenes/levels/LevelUI.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
 
 
+var level_node : Level
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	(get_parent() as Level).toy_exited_map.connect(update_score_ui)
-	pass # Replace with function body.
+	if get_parent() is Level :
+		level_node = get_parent()
+		if not level_node.is_node_ready() :
+			await level_node.ready
+		level_node.toy_exited_map.connect(update_score_ui)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,11 +20,16 @@ func update_score_ui(n_exited:int, min2exit:int) :
 	%ScoreLabel.text = str(n_exited)+'/'+str(min2exit)
 
 func _on_previous_level_pressed() -> void:
-	(get_parent() as Level).request_prev_level()
-	pass # Replace with function body.
+	if not level_node :
+		return
+	level_node.request_prev_level()
 
 func _on_next_level_pressed() -> void:
-	(get_parent() as Level).request_next_level()
+	if not level_node :
+		return
+	level_node.request_next_level()
 
 func _on_restart_level_pressed() -> void:
-	(get_parent() as Level).request_level_reload()
+	if not level_node :
+		return
+	level_node.request_level_reload()
