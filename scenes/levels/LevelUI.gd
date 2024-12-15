4ccd0ed45c2f7850403d811fc4d999c5578ed91f
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-15 23:45:03 +0000

    feat : level clear panel & automatic next level

--- scenes/levels/LevelUI.gd
+++ scenes/levels/LevelUI.gd
@@ -5,12 +5,14 @@ var level_node : Level
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	$LevelClearPanel.visible = false
 	if get_parent() is Level :
 		level_node = get_parent()
 		if not level_node.is_node_ready() :
 			await level_node.ready
 		level_node.toy_exited_map.connect(update_score_ui)
 		%LevelName.text = "Level "+str(level_node.get_lev_id())
+		level_node.level_passed.connect(_on_level_clear)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -34,3 +36,6 @@ func _on_restart_level_pressed() -> void:
 	if not level_node :
 		return
 	level_node.request_level_reload()
+
+func _on_level_clear(_n_exited:int, _min_to_exit:int) :
+	$AnimationPlayer.play('lev_clear')
