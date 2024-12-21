1e7c3b1d3944f8f417ee3d144d0a792c0dc48082
Ekoé Jean <70731505+JeanKouss@users.noreply.github.com>
2024-12-21 19:58:09 +0000

    feat : Levels list ui

---
+++ scenes/screens/level-list/LevelList.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+var lev_button_scene := preload("res://scenes/screens/level-list/LevelButton.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	init_lev_buttons()
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_lev_but_pressed(lev_num:int) :
+	LevelsController.reload_level(lev_num)
+
+func init_lev_buttons() :
+	for lev in LevelsController.levels :
+		var button : Button = lev_button_scene.instantiate()
+		button.text = 'Level ' + str(lev)
+		button.button_down.connect(_on_lev_but_pressed.bind(lev))
+		%LevelButtonsContainer.add_child(button)
